a63e02facdd525e6341897779caa3702acb9ba60
ahmad kabawa <backend01.arabinterface@gmail.com>
2025-03-12 08:50:21 -0300

    update patient

--- src/modules/patient/dto/create-patient.dto.ts
+++ src/modules/patient/dto/create-patient.dto.ts
@@ -1,62 +1,109 @@
-import { IsNotEmpty, IsOptional, IsString, IsDate, IsEnum, IsArray, ValidateNested, Min, Max } from 'class-validator';
-import { Type } from 'class-transformer';
-import { Types } from 'mongoose';
-
-class InsuranceDto {
+import {
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    IsDate,
+    IsEnum,
+    IsArray,
+    ValidateNested,
+    Min,
+    Max,
+  } from 'class-validator';
+  import { Type } from 'class-transformer';
+  import { Types } from 'mongoose';
+  
+  class EmergencyContact {
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+  
+    @IsNotEmpty()
+    @IsString()
+    phone: string;
+  
+  
+  }
+  class InsuranceDto {
     @IsNotEmpty()
     @IsString()
     insuranceProvider: string;
-
+  
     @IsNotEmpty()
     @IsString()
     insuranceNumber: string;
-
+  
     @IsNotEmpty()
     @Min(0)
     @Max(100)
     coveragePercentage: number;
-
+  
     @IsNotEmpty()
     @IsDate()
     expiryDate: Date;
-
+  
     @IsNotEmpty()
     @IsEnum(['private', 'governmental', 'corporate'])
     insuranceType: string;
-}
-
-export class CreatePatientDto {
+  }
+  
+  export class CreatePatientDto {
     @IsNotEmpty()
     @IsString()
     name: string;
-
+  
     @IsNotEmpty()
     @IsString()
     phone: string;
-
+  
+    @IsNotEmpty()
+    @IsString()
+    identity: string;
+  
     @IsNotEmpty()
     @IsDate()
     dateOfBirth: Date;
-
+  
     @IsNotEmpty()
     @IsEnum(['male', 'female'])
     gender: string;
-
+  
+    @IsNotEmpty()
+    @IsEnum(['Single', 'Married', 'Divorced'])
+    marital_status: string;
+  
+    @IsNotEmpty()
+    number_children: number;
+  
     @IsOptional()
     @IsString()
     email?: string;
-
+  
+    @IsOptional()
+    @IsString()
+    blood_type?: string;
+  
+    @IsOptional()
+    @IsString()
+    nationality?: string;
+  
     @IsOptional()
     @IsString()
     address?: string;
-
+  
     @IsOptional()
     @IsString()
     notes?: string;
-
+  
     @IsOptional()
     @IsArray()
     @ValidateNested({ each: true })
     @Type(() => InsuranceDto)
     insurances?: InsuranceDto[];
-}
+    
+    @IsOptional()
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => EmergencyContact)
+    emergencyContact?: EmergencyContact[];
+  }
+  
--- src/modules/patient/schemas/patient.schema.ts
+++ src/modules/patient/schemas/patient.schema.ts
@@ -5,79 +5,85 @@ export type PatientDocument = Patient & Document;
 
 @Schema({ timestamps: true })
 export class Patient {
-    _id: Types.ObjectId;
+  _id: Types.ObjectId;
 
-    @Prop({ required: true })
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  phone: string;
+
+  @Prop({ required: true })
+  dateOfBirth: Date;
+
+  @Prop({ required: true, enum: ['male', 'female'] })
+  gender: string;
+
+  @Prop()
+  identity?: string; // National ID or Passport
+
+  @Prop()
+  nationality?: string;
+
+  @Prop({ required: true, enum: ['Single', 'Married', 'Divorced'] })
+  marital_status?: string; // Example: "Single", "Married", "Divorced", etc.
+
+  @Prop()
+  number_children?: number;
+
+  @Prop()
+  blood_type?: string; // Example: "A+", "B-", "O+", etc.
+
+  @Prop()
+  height?: number; // in cm
+
+  @Prop()
+  weight?: number; // in kg
+
+  @Prop()
+  notes?: string;
+
+  @Prop()
+  email?: string;
+
+  @Prop()
+  address?: string;
+
+  @Prop({
+    type: {
+      name: { type: String, required: true },
+      phone: { type: String, required: true },
+    },
+    required: false, // Emergency contact is optional
+  })
+  emergencyContact?: {
     name: string;
-
-    @Prop({ required: true })
     phone: string;
+  };
 
-    @Prop({ required: true })
-    dateOfBirth: Date;
-
-    @Prop({ required: true, enum: ['male', 'female'] })
-    gender: string;
-
-    @Prop()
-    identity?: string; // National ID or Passport
-
-    @Prop()
-    nationality?: string;
-
-    @Prop()
-    marital_status?: string; // Example: "Single", "Married", "Divorced", etc.
-
-    @Prop()
-    number_children?: number;
-
-    @Prop()
-    blood_type?: string; // Example: "A+", "B-", "O+", etc.
-
-    @Prop()
-    height?: number; // in cm
-
-    @Prop()
-    weight?: number; // in kg
-
-    @Prop()
-    notes?: string;
-
-    @Prop()
-    email?: string;
-
-    @Prop()
-    address?: string;
-
-    @Prop({
-        type: {
-            name: { type: String, required: true },
-            phone: { type: String, required: true }
+  @Prop({
+    type: [
+      {
+        insuranceProvider: { type: String, required: true },
+        insuranceNumber: { type: String, required: true },
+        coveragePercentage: { type: Number, min: 0, max: 100, required: true },
+        expiryDate: { type: Date, required: true },
+        insuranceType: {
+          type: String,
+          enum: ['private', 'governmental', 'corporate'],
+          required: true,
         },
-        required: false, // Emergency contact is optional
-    })
-    emergencyContact?: {
-        name: string;
-        phone: string;
-    };
-
-    @Prop({
-        type: [{
-            insuranceProvider: { type: String, required: true },
-            insuranceNumber: { type: String, required: true },
-            coveragePercentage: { type: Number, min: 0, max: 100, required: true },
-            expiryDate: { type: Date, required: true },
-            insuranceType: { type: String, enum: ['private', 'governmental', 'corporate'], required: true }
-        }],
-        default: []
-    })
-    insurances: {
-        insuranceProvider: string;
-        insuranceNumber: string;
-        coveragePercentage: number;
-        expiryDate: Date;
-        insuranceType: string;
-    }[];
+      },
+    ],
+    default: [],
+  })
+  insurances: {
+    insuranceProvider: string;
+    insuranceNumber: string;
+    coveragePercentage: number;
+    expiryDate: Date;
+    insuranceType: string;
+  }[];
 }
 
 export const PatientSchema = SchemaFactory.createForClass(Patient);
