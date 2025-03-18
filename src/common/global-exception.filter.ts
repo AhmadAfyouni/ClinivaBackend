886afde1b86d0e20f7a9b65e8ce9e519d1a32be7
ahmad kabawa <backend01.arabinterface@gmail.com>
2025-03-18 05:03:22 -0300

    use GlobalExceptionFilter

---
+++ src/common/global-exception.filter.ts
@@ -0,0 +1,28 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+    catch(exception: any, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+
+        let status = HttpStatus.INTERNAL_SERVER_ERROR;
+        let message: any = 'Internal server error';
+
+        if (exception instanceof HttpException) {
+            status = exception.getStatus();
+            message = exception.getResponse(); // Directly take the NestJS response message
+        } else if (exception.code) {
+            // Handles database errors like unique constraints
+            status = HttpStatus.BAD_REQUEST;
+            message = exception.message || exception; // Returns the raw database error message
+        }
+
+        response.status(status).json({
+            statusCode: status,
+            message: message,
+            timestamp: new Date().toISOString(),
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {Logger} from '@nestjs/common';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-
+import {GlobalExceptionFilter} from "./common/global-exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalFilters(new GlobalExceptionFilter()); // Apply globally
+
     // Set Global API Prefix (e.g., /api/v1/)
     app.setGlobalPrefix('api/v1');
 
