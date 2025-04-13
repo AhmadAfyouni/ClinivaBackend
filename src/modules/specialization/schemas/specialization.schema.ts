df4629ca0ed977c8f624e965606384d89f71891d
ahmadafyouni <ahmad.afuni91@gmail.com>
2025-04-13 10:47:11 +0200

    add id to Specialization

--- src/modules/specialization/schemas/specialization.schema.ts
+++ src/modules/specialization/schemas/specialization.schema.ts
@@ -1,10 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type SpecializationDocument = Specialization & Document;
 
 @Schema({ timestamps: true })
 export class Specialization {
+  _id: Types.ObjectId;
+
   @Prop({ required: true })
   name: string;
 
