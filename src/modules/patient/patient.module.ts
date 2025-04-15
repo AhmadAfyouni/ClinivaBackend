a1b7c78d046b189de9927d0419ffde89367a3d3c
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-04-15 16:00:10 +0300

    done

--- src/modules/patient/patient.module.ts
+++ src/modules/patient/patient.module.ts
@@ -7,7 +7,7 @@ import { AppointmentModule } from '../appointment/appointment.module';
 import { MedicalRecordModule } from '../medicalrecord/medical-record.module';
 import { EmployeeModule } from '../employee/employee.module';
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),AppointmentModule,MedicalRecordModule],
+  imports: [MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),AppointmentModule,MedicalRecordModule,EmployeeModule],
   controllers: [PatientController],
   providers: [PatientService],
   exports: [PatientService], // يمكن استخدامها في وحدات أخرى
