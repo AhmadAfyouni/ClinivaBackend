4b607daacf00831b0d108ad12f0349742b72eb92
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-04-16 12:48:40 +0300

    done

--- src/modules/appointment/appointment.service.ts
+++ src/modules/appointment/appointment.service.ts
@@ -27,30 +27,52 @@ export class AppointmentService {
     };
   }
 
-  async getAllAppointments(
-    paginationDto: PaginationAndFilterDto,
-    filters: any,
-  ) {
+  async getAllAppointments(paginationDto: PaginationAndFilterDto, filters: any) {
     let { page, limit, allData, sortBy, order } = paginationDto;
-
+  
     // Convert page & limit to numbers
     page = Number(page) || 1;
     limit = Number(limit) || 10;
-
+  
     const sortField: string = sortBy ?? 'createdAt';
     const sort: Record<string, number> = {
       [sortField]: order === 'asc' ? 1 : -1,
     };
+  
+    const searchConditions: any[] = [];
+  
+    // تحقق إذا كان يوجد نص للبحث في الحقول النصية (doctor name, clinic name, patient name)
+    if (filters.search) {
+      const regex = new RegExp(filters.search, 'i'); // غير حساس لحالة الأحرف
+  
+      // إضافة شروط البحث للحقول النصية
+      searchConditions.push(
+        { 'doctor.name': regex },        // البحث في حقل اسم الطبيب
+        { 'clinic.name': regex },        // البحث في حقل اسم العيادة
+        { 'patient.name': regex },       // البحث في حقل اسم المريض
+      );
+    }
+  
+    // إزالة search من الفلاتر قبل تمريره
+    delete filters.search;
+  
+    // دمج الفلاتر مع شروط البحث
+    const finalFilter = {
+      ...filters,
+      ...(searchConditions.length > 0 ? { $or: searchConditions } : {}),
+    };
+  
     return paginate(
       this.appointmentModel,
       ['patient', 'clinic', 'doctor'],
       page,
       limit,
       allData,
-      filters,
+      finalFilter,
       sort,
     );
   }
+  
 
   async getAppointmentById(id: string): Promise<ApiGetResponse<Appointment>> {
     const appointment = await this.appointmentModel
