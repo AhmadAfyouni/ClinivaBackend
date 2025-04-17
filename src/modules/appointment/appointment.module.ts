ffd8aa3cdd105df7368ff7a8879408f49449b941
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-04-17 12:36:20 +0300

    done

--- src/modules/appointment/appointment.module.ts
+++ src/modules/appointment/appointment.module.ts
@@ -8,10 +8,11 @@ import { EmployeeModule } from '../employee/employee.module';
 import { PatientModule } from '../patient/patient.module';
 import { Employee,EmployeeSchema } from '../employee/schemas/employee.schema';
 import { Patient,PatientSchema } from '../patient/schemas/patient.schema';
+import { forwardRef } from '@nestjs/common';
 @Module({
   imports: [MongooseModule.forFeature([{ name: Appointment.name, schema: AppointmentSchema },{ name: Employee.name, schema: EmployeeSchema },
     
-     { name: Patient.name, schema: PatientSchema }]),PatientModule,EmployeeModule],
+     { name: Patient.name, schema: PatientSchema }]),forwardRef(() => PatientModule),forwardRef(() => EmployeeModule)],
   controllers: [AppointmentController],
   providers: [AppointmentService],
   exports: [AppointmentService,MongooseModule],
