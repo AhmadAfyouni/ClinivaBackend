b22d8cbf7a2177458240f307fcf71adf4787f045
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-04-20 14:35:35 +0300

    done

--- src/modules/appointment/appointment.service.ts
+++ src/modules/appointment/appointment.service.ts
@@ -149,7 +149,7 @@ export class AppointmentService {
         return { data: [], total: 0, page, limit, totalPages: 0 };
       }
     }
-    if (filters.datetime) {
+    if (filters.datetime ! == 'undefined' || filters.datetime ! == 'null') {
       const datetime = new Date(filters.datetime);
     
       if (!isNaN(datetime.getTime())) {
