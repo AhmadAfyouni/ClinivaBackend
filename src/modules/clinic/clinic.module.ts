adeb7e5d1c444b193efea788a7f23ed1c17b641f
AbdelwahapBakdones <developer01.arabinterface@gmail.com>
2025-04-21 16:33:27 +0300

    add stuff count from clinic

--- src/modules/clinic/clinic.module.ts
+++ src/modules/clinic/clinic.module.ts
@@ -4,9 +4,16 @@ import { ClinicService } from './clinic.service';
 import { ClinicController } from './clinic.controller';
 import { Clinic, ClinicSchema } from './schemas/clinic.schema';
 import { AppointmentSchema } from '../appointment/schemas/appointment.schema';
+import { Employee, EmployeeSchema } from '../employee/schemas/employee.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Clinic.name, schema: ClinicSchema }, { name: 'Appointment', schema: AppointmentSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Clinic.name, schema: ClinicSchema }, 
+      { name: 'Appointment', schema: AppointmentSchema },
+      { name: Employee.name, schema: EmployeeSchema }
+    ])
+  ],
   controllers: [ClinicController],
   providers: [ClinicService],
   exports: [ClinicService],
